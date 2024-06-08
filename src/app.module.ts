1e8ca2c618673f5997ad3885cf4f9d05e48b7241
ambrose-kibet <ambrosekibet36@gmail.com>
2024-06-08 14:05:44 +0300

    feat: Add AppModule for application module configuration

---
+++ src/app.module.ts
@@ -0,0 +1,46 @@
+import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { EmailModule } from './email/email.module';
+import * as Joi from 'joi'; //DO NOT CONVERT TO DEFAULT IMPORT
+
+@Module({
+  imports: [
+    TasksModule,
+    UserModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        JWT_ACCESS_TOKEN_SECRET: Joi.string().required(),
+        JWT_ACCESS_TOKEN_EXPIRATION_TIME: Joi.string().required(),
+        JWT_REFRESH_TOKEN_SECRET: Joi.string().required(),
+        JWT_REFRESH_TOKEN_EXPIRATION_TIME: Joi.string().required(),
+        GOOGLE_CLIENT_ID: Joi.string().required(),
+        GOOGLE_CLIENT_SECRET: Joi.string().required(),
+        GOOGLE_CALLBACK_URL: Joi.string().required(),
+        GITHUB_CLIENT_ID: Joi.string().required(),
+        GITHUB_CLIENT_SECRET: Joi.string().required(),
+        GITHUB_CALLBACK_URL: Joi.string().required(),
+        EMAIL_USER: Joi.string().required(),
+        EMAIL_PASSWORD: Joi.string().required(),
+        EMAIL_HOST: Joi.string().required(),
+        EMAIL_PORT: Joi.number().required(),
+        CLIENT_URL: Joi.string().required(),
+        JWT_EMAIL_SECRET: Joi.string().required(),
+        JWT_EMAIL_EXPIRATION_TIME: Joi.string().required(),
+        JWT_PASSWORD_SECRET: Joi.string().required(),
+        JWT_PASSWORD_EXPIRATION_TIME: Joi.string().required(),
+      }),
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    AuthModule,
+    EmailModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
