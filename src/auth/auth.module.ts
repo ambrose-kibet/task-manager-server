9d617d61fcb35c2e09385a0e43c330b01bef69f0
ambrose-kibet <ambrosekibet36@gmail.com>
2024-06-08 14:04:54 +0300

    feat: Add AuthModule for authentication functionality

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserModule } from '../user/user.module';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtRefreshTokenStrategy } from './strategies/jwt-refresh.strategy';
+import { GoogleAuthStrategy } from './strategies/google.strategy';
+import { GithubAuthStrategy } from './strategies/github-auth.strategy';
+import { DatabaseModule } from 'src/database/database.module';
+import { EmailModule } from 'src/email/email.module';
+import { TokenService } from './token.service';
+
+@Module({
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: process.env.JWT_ACCESS_TOKEN_SECRET,
+      signOptions: { expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME },
+    }),
+    DatabaseModule,
+    EmailModule,
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    JwtRefreshTokenStrategy,
+    GoogleAuthStrategy,
+    GithubAuthStrategy,
+    TokenService,
+  ],
+})
+export class AuthModule {}
