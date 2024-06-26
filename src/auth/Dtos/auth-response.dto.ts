ada3f639c2908c641a09b6a7f2c20011bb2315b4
ambrose-kibet <ambrosekibet36@gmail.com>
2024-06-26 16:02:39 +0300

    feat: Add ApiProperty decorators to AuthResponseDto properties

--- src/auth/Dtos/auth-response.dto.ts
+++ src/auth/Dtos/auth-response.dto.ts
@@ -1,11 +1,36 @@
 import { $Enums, User } from '@prisma/client';
 import { Exclude } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthResponseDto implements User {
+  @ApiProperty({
+    example: '1',
+    description: 'The id of the user',
+  })
   id: string;
+
+  @ApiProperty({
+    example: 'johndoe@mail.com',
+    description: 'The email of the user',
+  })
   email: string;
+
+  @ApiProperty({
+    example: 'John Doe',
+    description: 'The name of the user',
+  })
   name: string;
+
+  @ApiProperty({
+    example: 'USER',
+    description: 'The role of the user(ADMIN, USER)',
+  })
   role: $Enums.Role;
+
+  @ApiProperty({
+    example: 'https://www.example.com/avatar.jpg',
+    description: 'The avatar url of the user',
+  })
   avatar: string;
 
   @Exclude()
